eb7fd7ed95d6c2cf419484e2cbfc46aa839fedc0
Nikhil <nikhilsreeni@gmail.com>
2014-05-26 16:02:57 +0530

    BasicProject

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Dapper;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+namespace SampleConsole
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+           MyClass m=new MyClass();
+           m.testMethod();
+
+           Console.ReadLine();
+        }
+    }
+
+
+    class  MyClass : RepositoryBase
+    {
+        public void testMethod()
+        {
+
+            using (SqlConnection connection = this.Connection)
+            {
+                const string query = "SELECT * FROM [user] WHERE [user].srno=11";
+                Console.WriteLine(connection.Query<int>(query).FirstOrDefault() > 0 ? true : false);
+            }
+        }
+    }
+
+
+    public abstract partial class RepositoryBase
+    {
+        protected string ConnectiongString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["SqlConnectionString"].ConnectionString;
+            }
+        }
+
+        protected SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(this.ConnectiongString);
+            }
+        }
+    }
+
+}
+
